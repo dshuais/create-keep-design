75f61e92bfc3f9876fad9a9b9ad5c888abb9fcb2
dshuais <1137896420@qq.com>
2024-01-10 10:07:29 +0800

    feat：优化下载成功后log

--- packages/cli/utils/gitClone.js
+++ packages/cli/utils/gitClone.js
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-01-10 09:35:22
  * @LastEditors: dushuai
- * @LastEditTime: 2024-01-10 10:04:11
+ * @LastEditTime: 2024-01-10 10:06:38
  * @description: 模板下载命令
  */
 import download from 'download-git-repo'
@@ -24,8 +24,14 @@ export default (remote, name, option, tempIndex) => {
       }
       spinner.succeed(chalk.green(`模板下载成功！${tempIndex == 1 ? '快来开启你的个人组件库开发吧' : ''}\r\n`))
       console.log(chalk.blue(`cd ${name}\r\n`));
-      console.log(chalk.blue(`pnpm install\r\n`));
-      console.log(chalk.blue(`pnpm build:keep\r\n`));
+      if (tempIndex == 1) {
+        console.log(chalk.blue(`pnpm install\r\n`));
+        console.log(chalk.blue(`pnpm build:keep\r\n`));
+      } else {
+        console.log(chalk.blue(`pnpm install\r\n`));
+        console.log(chalk.blue(`pnpm dev\r\n`));
+      }
+
       resolve()
     })
   })
