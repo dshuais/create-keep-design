d9d1a8774b8be55b61e251c5dd5cbe76a87e23a6
dshuais <1137896420@qq.com>
2024-01-10 14:28:35 +0800

    新增husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,10 @@
+/*
+ * @Author: dushuai
+ * @Date: 2024-01-10 12:07:34
+ * @LastEditors: dushuai
+ * @LastEditTime: 2024-01-10 12:07:40
+ * @description: 心平气和
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+};
