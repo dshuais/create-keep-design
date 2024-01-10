e0f75d5da2c5804f825d3ced6a74a3c2502d6814
dushuais <1137896420@qq.com>
2024-01-10 20:49:14 +0800

    feat: 配置resolver搭建环境

--- packages/resolver/src/index.ts
+++ packages/resolver/src/index.ts
@@ -2,7 +2,7 @@
  * @Author: dushuai
  * @Date: 2024-01-09 16:29:26
  * @LastEditors: dushuai
- * @LastEditTime: 2024-01-10 18:44:41
+ * @LastEditTime: 2024-01-10 20:35:31
  * @description: 自动引入插件
  */
 
@@ -12,30 +12,30 @@ interface KeepDesignResolverOptions {
    *
    * @default 'esm'
    */
-  module?: 'esm' | 'cjs';
+  module?: "esm" | "cjs";
 }
 
 function KeepDesignResolver(options: KeepDesignResolverOptions) {
   return {
     type: "component",
     resolve: (name: string) => {
-      console.log('options:>> ', options);
-      console.log('name:>> ', name);
-      if (name.startsWith('K')) {
+      console.log("options:>> ", options);
+      console.log("name:>> ", name);
+      if (name.startsWith("K")) {
         const partialName = name.substring(1);
         // console.log('return:>> ', partialName, {
         //   name: 'K' + partialName,
         //   from: '@keep-design/components/src' // 这里应该是 k-design 的路径
         // });
         return {
-          name: 'K' + partialName,
-          from: '@keep-design/components/src' // 这里应该是 k-design 的路径
-        }
+          name: "K" + partialName,
+          from: "@keep-design/components/src", // 这里应该是 k-design 的路径
+        };
       }
-    }
-  }
+    },
+  };
 }
 
 module.exports = {
-  KeepDesignResolver
-}
+  KeepDesignResolver,
+};
